8ca96b2310e06326b187b227647f3b1615d4d2e5
boonyarit <boonyaritza93@hotmail.com>
2020-12-19 19:11:30 +0700

    add prompt Add & Request Tool

--- src/tools/components/MobileTool.js
+++ src/tools/components/MobileTool.js
@@ -1,34 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+
+import Modal from "../../shared/components/UIElements/Modal";
+import Input from "../../shared/components/FormElements/Input2";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
 
 
 function MobileTool(props) {
 
     const toolList = props.toolList;
 
+    const [showPrompt, setShowPrompt] = useState(false);
+    const [name, setName] = useState('');
+    const [modePrompt, setModePrompt] = useState(true);
+
+    const [formState, inputHandler] = useForm(
+        {
+            total: {
+                value: '',
+                isValid: false
+            },
+            description: {
+                value: '',
+                isValid: true
+            }
+        },
+        false
+    );
+
+    const cancelRequestHandler = () => {
+        setShowPrompt(false)
+    }
+
+    const openRequestHandler = (name) => {
+        setName(name);
+        setShowPrompt(true)
+        setModePrompt(true);
+    }
+
+    const openAddHandler = (name) => {
+        setName(name);
+        setShowPrompt(true)
+        setModePrompt(false);
+    }
+
+    const handleRequestForm = (e) => {
+        e.preventDefault();
+        setShowPrompt(false);
+        // console.log("success request: " + formState.inputs.total.value)
+    }
+
     return (
-        toolList.map((res) => (
-            <div className="mobile" key={res.id}>
-                <Link to={`detail/${res.id}`}><img src={res.imageProfile} alt="jpg" /></Link>
-                <h3>{res.toolName}</h3>
-                <div className="mobile-content">
-                    <div className="left">
-                        <h4>Type</h4>
-                        <h4>Status</h4>
-                        <h4>Piece</h4>
-                    </div>
-                    <div className="righ">
-                        <p>{res.type}</p>
-                        <p>{res.status}</p>
-                        <p>{res.total}</p>
+        <React.Fragment>
+            <Modal
+                show={showPrompt}
+                onCancel={cancelRequestHandler}
+                header={`${modePrompt ? 'Request' : 'Add'} ${name}`}
+                footerClass="place-item__modal-actions"
+                onSubmit={handleRequestForm}
+            >
+                <div className="prompt-request">
+                    <Input
+                        id="total"
+                        element="input"
+                        type="text"
+                        label="Total"
+                        errorText="Please enter a valid Tool."
+                        validators={[VALIDATOR_MINLENGTH(1)]}
+                        onInput={inputHandler}
+                    />
+                    <Input
+                        id="description"
+                        element="input"
+                        type="text"
+                        label="Description"
+                        errorText="Please enter a valid description."
+                        onInput={inputHandler}
+                        validators={[VALIDATOR_REQUIRE()]}
+
+                    />
+                    <div>
+                        <button type="submit" disabled={!formState.isValid} className="btn btn-submit" style={{ marginRight: "12px" }}>submit</button>
+                        <button type="button" onClick={cancelRequestHandler} className="btn btn-danger">cancel</button>
                     </div>
                 </div>
-                <div className="mobile-button">
-                    <button className="btn btn-submit">Request</button>
-                    <button className="btn btn-success">+Add</button>
+            </Modal>
+            {toolList.map((res) => (
+                <div className="mobile" key={res.id}>
+                    <Link to={`detail/${res.id}`}><img src={res.imageProfile} alt="jpg" /></Link>
+                    <h3>{res.nameTool}</h3>
+                    <div className="mobile-content">
+                        <div className="left">
+                            <h4>Type</h4>
+                            <h4>Status</h4>
+                            <h4>Piece</h4>
+                        </div>
+                        <div className="righ">
+                            <p>{res.type}</p>
+                            <p>{res.status}</p>
+                            <p>{res.total}</p>
+                        </div>
+                    </div>
+                    <div className="mobile-button">
+                        <button className="btn btn-submit" onClick={() => openRequestHandler(res.nameTool)}>Request</button>
+                        <button className="btn btn-success" onClick={() => openAddHandler(res.nameTool)}>+Add</button>
+                    </div>
                 </div>
-            </div>
-        ))
+            ))}
+        </React.Fragment>
     )
 }
 
